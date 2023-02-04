86441917c84599d5936ee194e05c02c587516cc7
Deurishhe <deurishhe@yandex.ru>
2023-02-04 23:07:39 +0300

    Added new commands and fixed other commands.

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -2,7 +2,9 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 from tgbot.keyboards.reply import main_menu
 from tgbot.funcs.queue import *
+from tgbot.funcs.photos import *
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -20,18 +22,126 @@ async def admin_start(message: Message):
 
 
 async def admin_help(message: Message):
-    await message.answer("Список команд:\n\n/users (список студентов)\n/subjects (список предметов)\n/queue (проверить очередь)")
+    await message.answer("Список команд:\n\n" +
+        "/users (список студентов)\n" +\
+        "/users_add (добавление пользователя, 4 параметра:\n\
+            \t1. ID в Телеграме\n\
+            \t2. Фамилия\n\
+            \t3. Имя\n\
+            \t4. Статус (например, student))\n" +\
+        "/users_del (удаление пользователя, 1 параметр:\n\
+            \t-> Номер позиции в списке пользователей)\n" +\
+        "/subjects (список предметов)\n" +\
+        "/queue (проверить очередь)\n" +\
+        "/queue_del (удаление позиции из очереди, 1 параметр:\n\
+            \t-> Номер позиции в полной очереди)\n" +\
+        "/queue_watch (просмотр позиции с доказательством, 1 параметр:\n\
+            \t-> Номер позиции в полной очереди)")
 
 
 async def admin_cmds(message: Message):
-    cmd = message.text.replace("/", "")
+    words = list(map(str, message.text.split()))
+    cmd = words[0].replace("/", "")
 
     if cmd == "users":
-        await message.answer(str(users_read()))
+        users = users_read()
+        msg = "Список пользователей:\n"
+
+        for ind, user in enumerate(users):
+            msg += "{}. {} {}: {}({})\n".format(ind + 1, users[user]['l_name'],\
+                users[user]['f_name'], user,users[user]['status'])
+
+        await message.answer(msg)
+    elif cmd == "users_add":
+        msg = ""
+        users = users_read()
+
+        if len(words) > 1 and words[1] in users:
+            msg = "Пользователь с данным ID уже есть в базе данных."
+
+        if not msg and len(words) == 5 and words[1].isdigit() and int(words[1]) != 0:
+            user = {"l_name": words[2], \
+                "f_name": words[3], \
+                "status": words[4]}
+            users[words[1]] = user
+
+            with open("./users_data/users_data.json", "w", encoding="utf-8") as file:
+                json.dump(users, file, indent=4, ensure_ascii=False)
+
+            msg = "Пользователь успешно добавлен"
+        elif not msg:
+            msg = "Вы ввели некорректные данные."
+
+        await message.answer(msg)
+    elif cmd == "users_del":
+        msg = "Вы ввели некорректные данные"
+
+        if len(words) == 2 and words[1].isdigit() and int(words[1]) != 0:
+            index_del = int(words[1]) - 1
+            users = users_read()
+
+            if (index_del >= len(users)):
+                msg = "Пользователя с таким номером не существует."
+            else:
+                users.pop(list(users)[index_del])
+
+                with open("./users_data/users_data.json", "w", encoding="utf-8") as file:
+                    json.dump(users, file, indent=4, ensure_ascii=False)
+
+                msg = "Удаление успешно завершено."
+
+        await message.answer(msg)
     elif cmd == "subjects":
-        await message.answer(str(subjects_read()))
+        subjects = subjects_read()
+        msg = "Список предметов:\n"
+
+        for ind, subject in enumerate(subjects):
+            msg += "{}. {}: {}\n".format(ind + 1, subject, subjects[subject])
+
+        await message.answer(msg)
     elif cmd == "queue":
-        await message.answer(str(queue_read()))
+        msg = "Полная очередь:\n"
+        full_queue = queue_msg(None, None)
+
+        if not full_queue:
+            full_queue = "Очередь пуста."
+
+        msg += full_queue
+
+        await message.answer(msg)
+    elif cmd == "queue_del":
+        msg = "Вы ввели некорректные данные"
+
+        if len(words) == 2 and words[1].isdigit() and int(words[1]) != 0:
+            index_del = int(words[1]) - 1
+            queue_req = queue_read()
+
+            if (index_del >= len(queue_req)):
+                msg = "Позиции с таким номером не существует."
+            else:
+                queue_del(queue_req[index_del][0], queue_req[index_del][1], queue_req[index_del][2])
+                os.remove(photo_name(queue_req[index_del][0], queue_req[index_del][1], queue_req[index_del][3]))
+
+                msg = "Удаление успешно завершено."
+
+        await message.answer(msg)
+    elif cmd == "queue_watch":
+        msg = "Вы ввели некорректные данные"
+
+        if len(words) == 2 and words[1].isdigit() and int(words[1]) != 0:
+            index_del = int(words[1]) - 1
+            queue_req = queue_read()
+
+            if (index_del >= len(queue_req)):
+                msg = "Позиции с таким номером не существует."
+                await message.answer(msg)
+            else:
+                name = photo_name(queue_req[index_del][0], queue_req[index_del][1], queue_req[index_del][3])
+                msg = list(map(str, queue_msg(None, None).split("\n")))[index_del]
+
+                await message.answer_photo(open(name, 'rb'), caption=msg)
+        else:
+            await message.answer(msg)
 
 
 def register_admin(dp: Dispatcher):
@@ -40,4 +150,5 @@ def register_admin(dp: Dispatcher):
     dp.register_message_handler(admin_help, commands=[
                                 "help"], state="*", is_admin=True)
     dp.register_message_handler(admin_cmds, commands=[
-                                "users", "subjects", "queue"], state="*", is_admin=True)
+                                "users", "users_add", "users_del", "subjects", "queue",
+                                "queue_del", "queue_watch"], state="*", is_admin=True)
